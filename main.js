0e59e5aa15a7b7342acf9a2514828c248814527c
Ami Fletcher <amifletcher@Amis-iMac.local>
2021-11-16 15:36:51 +0000

    HTML boilerplate, grid and buttons added. Starting on the styles.

---
+++ main.js
@@ -0,0 +1,14 @@
+// Have a grid, with shown, pre selected icons in it
+// pre selected icons are greyed out slighty, inputtable squares are white.
+
+// when player figures out a new incon placement-
+// they select the square and then the icon to go into that square
+
+// there will be buttons to erase an answer and a button to check the answers
+// incorrect answers will be highlighted red until a new input.
+
+// Upon any input squares change to white colour
+
+// can add extra challanges in new HTML pages
+
+
