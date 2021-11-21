45569dcc55e0f5c36879a37c74c754904d88287a
Ami Fletcher <amifletcher@Amis-iMac.local>
2021-11-21 02:07:54 +0000

    Changed variables to use class name for i tag in order to properly access the HTML. Removed the ID tags from icon__buttons as no longer needed. Applied button on click to tiles

--- main.js
+++ main.js
@@ -2,14 +2,17 @@
 // select the icons and tiles to be used
 const elementIcons = document.querySelectorAll(".icons_button");
 const tiles = document.querySelectorAll(".input");
-const fireIcon = document.getElementById("#fire-icon");
-const windIcon = document.getElementById("wind-icon");
-const waveIcon = document.getElementById("wave-icon");
-const earthIcon = document.getElementById("earth-icon");
-const eraseButton = document.querySelector(".erase");
+const fireIcon = document.querySelector(".fire");
+const windIcon = document.querySelector(".wind");
+const waveIcon = document.querySelector(".wave");
+const earthIcon = document.querySelector(".earth");
+// const eraseButton = document.querySelector(".erase");
 const checkButton = document.querySelector(".check");
 const resetButton = document.querySelector(".reset");
+// const iconButtons = document.querySelectorAll(".icons__button")
 
+
+console.log(elementIcons);
 
 for (let i = 0; i < tiles.length; i++) {
     const tile = tiles[i];
@@ -23,10 +26,17 @@ for (let i = 0; i < tiles.length; i++) {
     const tile = tiles[i];
     tile.addEventListener("click", () => {
         tile.classList.toggle("active");
+    if (fireIcon.classList.contains("fire")) {
+        tile.appendChild(fireIcon); 
+    }
 });
+
 };
 
 
+
+
+// When the erase button is clicked, it removes the html from the selected tile
 
 // when player figures out a new icon placement-
 // they select the square and then the icon to go into that square
